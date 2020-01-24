46dc8c72f88e77ee4770c9404cd118e752b6c0d8
Bruna Seewald <bruna.seewald@gmail.com>
2020-01-23 21:02:40 -0300

    Added: temperature sensor and http communication

--- mws-main/mws-main.ino
+++ mws-main/mws-main.ino
@@ -19,6 +19,9 @@
 
 #include <WiFi.h>
 #include <Ezo_i2c.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "esp_http_client.h"
 
 // ---------------------------------------------
 // Debug options
@@ -40,10 +43,24 @@ const char* ssid     = "PROJETOAUTOFL";
 const char* password = "******";
 
 // ---------------------------------------------
-// I2C address and name
+// Server URL
 // ---------------------------------------------
-Ezo_board PH = Ezo_board(99, "PH");
-Ezo_board EC = Ezo_board(100, "EC");
+const char *post_url = "http://your-webserver.net/yourscript.php";
+
+// ---------------------------------------------
+// Ph and EC - I2C address
+// ---------------------------------------------
+// Ezo_board PH = Ezo_board(99, "PH");
+// Ezo_board EC = Ezo_board(100, "EC");
+
+// ---------------------------------------------
+// DS18B20 Temperature Sensor
+// ---------------------------------------------
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature temp_sensors(&oneWire);
+DeviceAddress temp_sensor_feed = { 0x28, 0x92, 0x98, 0x79, 0x97, 0x7, 0x3, 0xAD };
+DeviceAddress temp_sensor_permeate = { 0x28, 0x26, 0x2B, 0x79, 0x97, 0x2, 0x3, 0x88 };
 
 // ---------------------------------------------
 // Variables
@@ -55,14 +72,15 @@ int return_code = 0;
 unsigned long next_poll_time = 0;
 
 const unsigned long reading_delay = 1000;     // how long we wait to receive a response, in milliseconds
-const unsigned long loop_delay = 300000;      // collect loop time: 5min
+// const unsigned long loop_delay = 300000;      // collect loop time: 5min
+const unsigned long loop_delay = 15000;
 
 
 void setup()
 {
-  // I2C and serial ports
-  Wire.begin();
+  // SerialPrint
   Serial.begin(115200);
+  delay(10);
 
   // WiFi network
   while (!Serial) ;
@@ -78,72 +96,149 @@ void setup()
   DEBUG_PRINTLN("WiFi connected");
   DEBUG_PRINTLN("IP address: ");
   DEBUG_PRINTLN(WiFi.localIP());
+
+  // Temp
+  temp_sensors.begin();
+  // I2C
+  // Wire.begin();
 }
 
 void loop()
 {
+  String values = "";
+
   // Collect info from sensors
   switch(current_step) {
     case REQUEST:
       if (millis() >= next_poll_time) {
-        // TODO: temperature
+        // Send the command to get temperature, pH and EC values
+        temp_sensors.requestTemperatures();
+        // PH.send_read_cmd();
+        // EC.send_read_cmd();
 
-        PH.send_read_cmd();
-        EC.send_read_cmd();
-
-        next_poll_time = millis() + reading_delay;         //set when the response will arrive
+        // Set when the response will arrive
+        next_poll_time = millis() + reading_delay;
         current_step = READ;
       }
       break;
 
     case READ:
       if (millis() >= next_poll_time) {
+        // Read temperature, pH and EC
+        int feed = temp_sensors.getTempC(temp_sensor_feed);
+        int permeate = temp_sensors.getTempC(temp_sensor_permeate);
+        DEBUG_PRINT("FEED: "); DEBUG_PRINTLN(feed);
+        DEBUG_PRINT("PERMEATE: "); DEBUG_PRINTLN(permeate);
+        //TODO: put value in URL API
+        values += "tempfeed=" + String(feed) + "&temppermeate=" + String(permeate);
 
-        PH.receive_read_cmd();
-        DEBUG_PRINT(PH.get_name()); DEBUG_PRINT(": ");
+        // PH.receive_read_cmd();
+        // DEBUG_PRINT(PH.get_name()); DEBUG_PRINT(": ");
 
-        if(reading_succeeded(PH) == true){                     // if the pH reading has been received and it is valid
-          DEBUG_PRINTLN(PH.get_last_received_reading(), 2);
-          //TODO: put value in URL API
-        }
+        // if(reading_succeeded(PH) == true){                     // if the pH reading has been received and it is valid
+        //   DEBUG_PRINTLN(PH.get_last_received_reading(), 2);
+        //   //TODO: put value in URL API
+        //   values += "&ph=" + String(PH.get_last_received_reading(), 2);
+        // }
 
-        EC.receive_read_cmd();
-        DEBUG_PRINT(EC.get_name()); DEBUG_PRINT(": ");
+        // EC.receive_read_cmd();
+        // DEBUG_PRINT(EC.get_name()); DEBUG_PRINT(": ");
 
-        if(reading_succeeded(EC) == true){                    // if the EC reading has been received and it is valid
-          DEBUG_PRINTLN(EC.get_last_received_reading(), 0);
-          //TODO: put value in URL API
-        }
+        // if(reading_succeeded(EC) == true){                    // if the EC reading has been received and it is valid
+        //   DEBUG_PRINTLN(EC.get_last_received_reading(), 0);
+        //   //TODO: put value in URL API
+        //   values += "&ec=" + String(EC.get_last_received_reading(), 2);
+        // }
 
         // Next collect: +5 min
         next_poll_time =  millis() + loop_delay;              // update the time for the next reading loop
         current_step = REQUEST;
       }
+      // TODO: send URL to a server
+      DEBUG_PRINTLN(values);
+      post_data_server(values.c_str());
       break;
   }
-
-    // TODO: send URL to a server
 }
 
-bool reading_succeeded(Ezo_board &Sensor) {                  // this function makes sure that when we get a reading we know if it was valid or if we got an error
 
+// this function makes sure that when we get a reading
+// we know if it was valid or if we got an error
+bool reading_succeeded(Ezo_board &Sensor) {
   switch (Sensor.get_error()) {
     case Ezo_board::SUCCESS:
       return true;
 
     case Ezo_board::FAIL:
-      Serial.print("Failed ");
+      DEBUG_PRINTLN("Failed");
       return false;
 
-    case Ezo_board::NOT_READY:                              // if the reading was taken to early, the command has not yet finished calculating
-      Serial.print("Pending ");
+    case Ezo_board::NOT_READY:     // if the reading was taken to early, the command has not yet finished calculating
+      DEBUG_PRINTLN("Pending");
       return false;
 
     case Ezo_board::NO_DATA:
-      Serial.print("No Data ");
+      DEBUG_PRINTLN("No Data");
       return false;
 
-    default:                                                // if none of the above happened
+    default:                      // if none of the above happened
      return false;
   }
+}
+
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+  switch (evt->event_id) {
+    case HTTP_EVENT_ERROR:
+      DEBUG_PRINTLN("HTTP_EVENT_ERROR");
+      break;
+    case HTTP_EVENT_ON_CONNECTED:
+      DEBUG_PRINTLN("HTTP_EVENT_ON_CONNECTED");
+      break;
+    case HTTP_EVENT_HEADER_SENT:
+      DEBUG_PRINTLN("HTTP_EVENT_HEADER_SENT");
+      break;
+    case HTTP_EVENT_ON_HEADER:
+      DEBUG_PRINTLN("HTTP_EVENT_ON_HEADER");
+      // DEBUG_PRINTLN("HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+      break;
+    case HTTP_EVENT_ON_DATA:
+      DEBUG_PRINTLN("HTTP_EVENT_ON_DATA");
+      // DEBUG_PRINTLN("HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+      // if (!esp_http_client_is_chunked_response(evt->client)) {
+      //   // Write out data
+      //   printf("%.*s", evt->data_len, (char*)evt->data);
+      // }
+      break;
+    case HTTP_EVENT_ON_FINISH:
+      DEBUG_PRINTLN("HTTP_EVENT_ON_FINISH");
+      break;
+    case HTTP_EVENT_DISCONNECTED:
+      DEBUG_PRINTLN("HTTP_EVENT_DISCONNECTED");
+      break;
+  }
+  return ESP_OK;
+}
+
+static esp_err_t post_data_server(const char *post_data)
+{
+  esp_err_t res = ESP_OK;
+  esp_http_client_handle_t http_client;
+  esp_http_client_config_t config_client = {0};
+
+  config_client.url = post_url;
+  config_client.event_handler = _http_event_handler;
+  config_client.method = HTTP_METHOD_POST;
+
+  http_client = esp_http_client_init(&config_client);
+
+  esp_http_client_set_post_field(http_client, post_data, strlen(post_data));
+
+  esp_err_t err = esp_http_client_perform(http_client);
+  if (err == res) {
+    DEBUG_PRINT("esp_http_client_get_status_code: ");
+    DEBUG_PRINTLN(esp_http_client_get_status_code(http_client));
+  }
+  esp_http_client_cleanup(http_client);
 }
